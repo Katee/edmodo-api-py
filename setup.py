204e33eda21804e4dd33d312cc5bb5937734050c
katee <hello@kate.io>
2014-03-13 10:54:04 -0400

    Simple setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='EdmodoApi',
+    version='0.1.0',
+    author='',
+    author_email='',
+    packages=['edmodoapi'],
+    description='Simple wrapper for the Edmodo API',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "requests>= 2.2.1",
+    ],
+)
